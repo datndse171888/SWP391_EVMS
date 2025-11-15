9c6dd33eab58640d111d676562fc404f3a000588
datndse171888 <datndse171888@fpt.edu.vn>
2025-11-16 00:15:20 +0700

    fix booking service UI: choose service

--- EVMS_FE/src/pages/booking/Booking.tsx
+++ EVMS_FE/src/pages/booking/Booking.tsx
@@ -158,7 +158,7 @@ const Booking: React.FC = () => {
     }
 
     return (
-        <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-md mt-22 mb-10">
+        <div className="max-w-6xl mx-auto p-6 bg-white rounded-lg shadow-md mt-22 mb-10">
             <ProcessBar currentStep={step} steps={steps} />
             {renderStep()}
             {AlertComponent}
